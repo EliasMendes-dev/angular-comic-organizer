3fab5c93bc19779429305496a7cb78087ce399b7
Zubizeru <jose.ehmendes@senacsp.edu.br>
2026-05-27 10:46:25 -0300

    feat: enhance home layout with responsive design and split areas for better organization

--- src/app/pages/home/home.ts
+++ src/app/pages/home/home.ts
@@ -1,17 +1,34 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, HostListener } from '@angular/core';
 import { FileExplorer } from '../../components/file-explorer/file-explorer';
 import { ComicPreview } from '../../components/comic-preview/comic-preview';
 import { RenameSettings } from '../../components/rename-settings/rename-settings';
 import { SplitComponent, SplitAreaComponent } from 'angular-split';
+import {LucideGrip} from '@lucide/angular';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [FileExplorer, ComicPreview, RenameSettings, SplitComponent, SplitAreaComponent],
+  imports: [
+    FileExplorer,
+    ComicPreview,
+    RenameSettings,
+    LucideGrip,
+    SplitComponent,
+    SplitAreaComponent
+  ],
   templateUrl: './home.html',
   styleUrls: ['./home.css', './home-responsive.css'],
 })
 export class Home implements OnInit {
+  isDesktop = window.innerWidth > 1000;
+
+  /* Host listener vai atualizar o estado quando a janela for redimensionada, no geral ele serve para detectar mudanças no tamanho da janela */
+  @HostListener('window:resize')
+  onResize() {
+    this.isDesktop = window.innerWidth > 1000;
+  }
+
   constructor() {}
+
   ngOnInit(): void {}
 }
